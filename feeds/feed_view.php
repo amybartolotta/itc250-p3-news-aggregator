e1b7193b6e7bb7e958361d58a6dcee1a6cbb8e87
kdaniel32 <kevindaniel14@outlook.com>
2017-06-08 13:27:18 -0700

    adding feed view page stub
    this version does not display the RSS or do caching

---
+++ feeds/feed_view.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * survey_view.php is a page to demonstrate the proof of concept of the 
+ * initial SurveySez objects.
+ *
+ * Objects in this version are the Survey, Question & Answer objects
+ * 
+ * @package SurveySez
+ * @author William Newman
+ * @version 2.12 2015/06/04
+ * @link http://newmanix.com/ 
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @see Question.php
+ * @see Answer.php
+ * @see Response.php
+ * @see Choice.php
+ */
+ 
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
+spl_autoload_register('MyAutoLoader::NamespaceLoader');//required to load SurveySez namespace objects
+$config->metaRobots = 'no index, no follow';#never index survey pages
+
+# check variable of item passed in - if invalid data, forcibly redirect back to demo_list.php page
+if(isset($_GET['id']) && (int)$_GET['id'] > 0){#proper data must be on querystring
+	 $myID = (int)$_GET['id']; #Convert to integer, will equate to zero if fails
+}else{
+	myRedirect(VIRTUAL_PATH . "feeds/index.php");
+}
+
+
+# SQL statement
+$sql = "SELECT * FROM `sp17_Feeds` WHERE `FeedID` = $myID;";
+
+/*
+$mySurvey = new SurveySez\Survey($myID); //MY_Survey extends survey class so methods can be added
+if($mySurvey->isValid)
+{
+	$config->titleTag = "'" . $mySurvey->Title . "' Survey!";
+}else{
+	$config->titleTag = smartTitle(); //use constant 
+}
+*/
+
+#END CONFIG AREA ---------------------------------------------------------- 
+
+get_header(); #defaults to theme header or header_inc.php
+?>
+
+<h3 align="center">News Feed</h3>
+
+<p>This is the page where we are going to do the caching and display the RSS feed.  This is just a placeholde to be sure we are getting the right record from the database.</p>
+
+<?php
+#reference images for pager
+$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
+$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
+
+# Create instance of new 'pager' class
+$myPager = new Pager(10,'',$prev,$next,'');
+$sql = $myPager->loadSQL($sql);  #load SQL, add offset
+
+# connection comes first in mysqli (improved) function
+$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+
+
+if(mysqli_num_rows($result) > 0)
+{#records exist - process
+	if($myPager->showTotal()==1){$itemz = "feed";}else{$itemz = "feeds";}  //deal with plural
+    echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
+	
+	echo '
+	
+	<table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th>Feed Name</th>
+      <th>Feed Description</th>
+	  <th>Feed Link</th>
+    </tr>
+  </thead>
+  <tbody>
+	
+	';
+
+	
+	while($row = mysqli_fetch_assoc($result))
+	{# process each row
+         #echo '<div align="center"><a href="' . VIRTUAL_PATH . 'feeds/feed_view.php?id=' . (int)$row['FeedID'] . '">' . dbOut($row['FeedTitle']) . '</a>';
+         #echo '</div>';
+		 		
+		echo '
+			<tr>
+				<td>' . dbOut($row['FeedTitle']) . '</a></td>
+				<td>' . dbOut($row['FeedDescription']) . '</td>
+				<td>' . dbOut($row['FeedLink']) . '</td>
+			</tr>
+		';
+
+	}
+	
+	
+	
+	echo '
+	
+		  </tbody>
+	</table> 
+	
+	';
+	
+	echo $myPager->showNAV(); # show paging nav, only if enough records	 
+}else{#no records
+    echo "<div align=center>What! No feeds?  There must be a mistake!!</div>";	
+}
+@mysqli_free_result($result);
+
+get_footer(); #defaults to theme footer or footer_inc.php
+?>
