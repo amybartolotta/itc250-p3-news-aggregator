9595c33b44c03debe93f943e19d1c2485e818199
kdaniel32 <kevindaniel14@outlook.com>
2017-06-08 16:19:35 -0700

    Fixed display of RSS and removed unnecessary code
    Still does not include caching and session code

--- feeds/feed_view.php
+++ feeds/feed_view.php
@@ -5,15 +5,12 @@
  *
  * Objects in this version are the Survey, Question & Answer objects
  * 
- * @package SurveySez
- * @author William Newman
- * @version 2.12 2015/06/04
- * @link http://newmanix.com/ 
+ * @package 
+ * @author 
+ * @version 
+ * @link 
  * @license http://www.apache.org/licenses/LICENSE-2.0
- * @see Question.php
- * @see Answer.php
- * @see Response.php
- * @see Choice.php
+ * @see 
  */
  
 require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
@@ -46,18 +43,22 @@ if($mySurvey->isValid)
 get_header(); #defaults to theme header or header_inc.php
 ?>
 
+<!--
 <h3 align="center">News Feed</h3>
 
 <p>This is the page where we are going to do the caching and display the RSS feed.  This is just a placeholde to be sure we are getting the right record from the database.</p>
+-->
 
 <?php
+
+# there should only be one row so we don't need the pager
 #reference images for pager
-$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
-$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
+#$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
+#$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
 
 # Create instance of new 'pager' class
-$myPager = new Pager(10,'',$prev,$next,'');
-$sql = $myPager->loadSQL($sql);  #load SQL, add offset
+#$myPager = new Pager(10,'',$prev,$next,'');
+#$sql = $myPager->loadSQL($sql);  #load SQL, add offset
 
 # connection comes first in mysqli (improved) function
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
@@ -65,52 +66,30 @@ $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::
 
 if(mysqli_num_rows($result) > 0)
 {#records exist - process
-	if($myPager->showTotal()==1){$itemz = "feed";}else{$itemz = "feeds";}  //deal with plural
-    echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
 	
-	echo '
+	# there should only be one row so I don't think we need a loop
+	$row = mysqli_fetch_assoc($result);
 	
-	<table class="table table-striped table-hover ">
-  <thead>
-    <tr>
-      <th>Feed Name</th>
-      <th>Feed Description</th>
-	  <th>Feed Link</th>
-    </tr>
-  </thead>
-  <tbody>
+	#do the RSS stuff
+	$request = dbOut($row['FeedLink']);
+	$response = file_get_contents($request);
+	$xml = simplexml_load_string($response);
 	
-	';
-
-	
-	while($row = mysqli_fetch_assoc($result))
-	{# process each row
-         #echo '<div align="center"><a href="' . VIRTUAL_PATH . 'feeds/feed_view.php?id=' . (int)$row['FeedID'] . '">' . dbOut($row['FeedTitle']) . '</a>';
-         #echo '</div>';
-		 		
-		echo '
-			<tr>
-				<td>' . dbOut($row['FeedTitle']) . '</a></td>
-				<td>' . dbOut($row['FeedDescription']) . '</td>
-				<td>' . dbOut($row['FeedLink']) . '</td>
-			</tr>
-		';
-
+	echo '<h3 align="center">' . $xml->channel->title . '</h3>';
+  
+	foreach($xml->channel->item as $story)
+	{
+	    echo '<table class="table table-striped table-hover ">';
+		echo '<thead><tr><td><a href="' . $story->link . '">' . $story->title . '</a></td></tr></thead>'; 
+		echo '<tr><td>' . $story->description . '</td></tr>';
+		echo '</table>';
 	}
-	
-	
-	
-	echo '
-	
-		  </tbody>
-	</table> 
-	
-	';
-	
-	echo $myPager->showNAV(); # show paging nav, only if enough records	 
+
 }else{#no records
     echo "<div align=center>What! No feeds?  There must be a mistake!!</div>";	
-}
+}#end if(mysqli_num_rows($result) > 0)
+
+
 @mysqli_free_result($result);
 
 get_footer(); #defaults to theme footer or footer_inc.php
